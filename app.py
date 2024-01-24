565e1a1149d60c6ca8b144363ac4cd7094d676bb
Colby <computertech312@gmail.com>
2024-01-24 02:56:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -198,4 +198,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables
         create_default_admin_user()  # Create the default admin user
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8888, debug=True)
