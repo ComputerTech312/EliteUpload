da19625cabe405ef572ed71ab2296b53f31e1caa
ComputerTech312 <computertech312@gmail.com>
2024-01-26 21:49:49 +0100

    test

--- static/script.js
+++ static/script.js
@@ -4,13 +4,26 @@ document.getElementById('upload-form').addEventListener('submit', function(event
     var uploadURL = document.getElementById('upload-url');
     uploadURL.textContent = 'Uploading...';
 
+    var progressBar = document.getElementById('progress-bar'); // Assuming you have a progress bar element
+
     fetch('/upload', {
         method: 'POST',
         body: formData
     })
-    .then(response => response.text())
-    .then(data => {
-        uploadURL.innerHTML = 'File URL: <a href="' + data + '" target="_blank">' + data + '</a>';
+    .then(response => {
+        const reader = response.body.getReader();
+        const totalLength = +response.headers.get('Content-Length');
+
+        reader.read().then(function process({done, value}) {
+            if (done) {
+                return uploadURL.innerHTML = 'File URL: <a href="' + data + '" target="_blank">' + data + '</a>';
+            }
+
+            progressBar.value += value.length;
+            progressBar.max = totalLength;
+
+            return reader.read().then(process);
+        });
     })
     .catch(error => {
         console.error('Error:', error);
